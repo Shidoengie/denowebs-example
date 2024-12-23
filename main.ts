ecdf3683fa3ff981c0b8f4cd201aa4ea1835d524
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2024-12-23 23:13:21 +0000

    Made changes to pages appear on reload, improved login.

--- main.ts
+++ main.ts
@@ -5,26 +5,25 @@ import { MimeType } from "./src/mimetypes.ts"
 if (!import.meta.main) {
   throw new Error("cannot be used as library");
 }
-
-view("/error.hbs").then((view) => router.setErrorPage(view));
-
-view("/view.hbs").then((view) =>
-  router.templ("/", view, () => ({
+async function getRoutes() {
+  const [error,home] = await Promise.all([view("/error.hbs"),view("/view.hbs")])
+  router.setErrorPage(error)
+  router.templ("/", home, () => ({
     test: Math.random() * 10000,
   }))
-);
-router.any("/test/",(req,groups)=>{
-  const params = new URL(req.url).searchParams;
-  let buffer = JSON.stringify(Array.from(params.entries()))
-  return new Response(buffer, {
-    headers: {
-      "content-type": MimeType.Json,
-      
-    }
+  router.any("/test/",(req,groups) => {
+    const params = new URL(req.url).searchParams;
+    let buffer = JSON.stringify(Array.from(params.entries()))
+    return new Response(buffer, {
+      headers: {
+        "content-type": MimeType.Json,
+        
+      }
+    })
   })
-})
-Deno.serve((req) => {
+}
+Deno.serve(async (req) => {
   registerContent();
-
+  await getRoutes()
   return router.serve(req);
 });
