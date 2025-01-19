31acf0d4e348f151673ae44ed95c70bb7c3f8e73
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2025-01-19 11:45:45 +0000

    removed lib files

--- cmd/migration.ts
+++
@@ -1,9 +0,0 @@
-import config from "../config/paths.ts" with { type: "json" };
-async function main(): Promise<void> {
-  const date = new Date();
-  const fileName = `${config.migrations}/${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}__${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}.sql`;
-  await Deno.create(fileName);
-}
-if (import.meta.main) {
-  main();
-}
